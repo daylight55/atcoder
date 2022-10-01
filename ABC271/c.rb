47f61248c1dc51adf266dd226e4f77b4955d74c6
daylight55 <jojomasanori3@gmail.com>
2022-10-01 22:05:50 +0900

    abc271: wip c

---
+++ ABC271/c.rb
@@ -0,0 +1,12 @@
+#!/bin/env ruby
+
+N = gets.chomp.to_i
+c = gets.chomp.split.to_i
+
+MAX = 10 ** 9
+readable_list = Array.new(MAX)
+hold_list = c.sort
+
+readable_list.each_with_index do |n, i|
+  
+end
