4ec8ba5eb00c3f60b396bc2296611746335c5837
daylight55 <jojomasanori3@gmail.com>
2022-10-22 22:05:22 +0900

    abc274: hash c

--- ABC274/c.rb
+++ ABC274/c.rb
@@ -3,18 +3,65 @@
 N = gets.chomp.to_i
 A = gets.chomp.split.map(&:to_i)
 # 系譜
-genealogies = []
+genealogies = Hash.new { |h,k| h[k] = {} }
 
 A.each_with_index { |a, i|
   # parent, child1, child2の順
-  genealogies.append([a, 2*(i + 1), 2*(i + 1) + 1])
+  genealogies[i]["parent"] = a
+  genealogies[i]["child"] = [2*(i + 1), 2*(i + 1) + 1]
 }
-# アメーバのリスト
-amoebae = genealogies.flatten.uniq
-pp amoebae
 
 ans = Array(2 * N + 1)
+
+
 genealogies.each{ |genealogy|
-  parent = genealogy[0]
+
   puts parent
 }
+
+
+########################
+# 連結リスト
+########################
+class List
+  class Cell
+    attr_accessor :val
+    attr_accessor :next
+
+    def initialize(val = nil)
+      @val = val
+      @next = nil
+    end
+  end
+
+  def initialize()
+    @num = 0
+    @head = nil
+    @tail = @head
+  end
+
+  def push(val)
+    if @head.nil?
+      @tail = @head = Cell.new(val)
+    else
+      @tail.next = Cell.new(val)
+      @tail = @tail.next
+    end
+
+    @num += 1
+  end
+
+  def to_a()
+    arr = Array.new(@num)
+
+    i = 0
+    cell = @head
+    while cell
+      arr[i] = cell.val
+      cell = cell.next
+      i += 1
+    end
+
+    return arr
+  end
+end
