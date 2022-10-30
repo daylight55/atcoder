c841741f3983beef8f651dede87f908d723a12ee
daylight55 <jojomasanori3@gmail.com>
2022-10-30 13:07:26 +0900

    fix

--- ABC273/c.rb
+++ ABC273/c.rb
@@ -2,20 +2,10 @@
 
 N = gets.chomp.to_i
 A = gets.chomp.split.map(&:to_i)
-r = A.sort.uniq
-h = r.map { |x| [x, nil] }.to_h
-ans = Array.new(N, 0)
 
-for i in 0..N-1 do
-  larger_num = r.size - r.index(i) - 1
-  h[i] = larger_num
-end
+T = A.tally
+pp T
+pp *T.keys.sort.reverse
+pp T.values_at(*T.keys.sort.reverse)
 
-for i in 0..N-1 do
-  ans[i] = h[A[i]]
-end
-
-for k in 0..N-1 do
-  t = ans.tally[k] || 0
-  puts t
-end
+# puts T.values_at(*T.keys.sort.reverse),[0]*(N-T.size)
