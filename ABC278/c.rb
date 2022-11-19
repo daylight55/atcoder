b42e4eaf1f7cc64e774c2eb875158d711ad4e130
daylight55 <jojomasanori3@gmail.com>
2022-11-19 22:38:20 +0900

    abc278: TLE c

--- ABC278/c.rb
+++ ABC278/c.rb
@@ -1,27 +1,48 @@
-n, q = gets.chomp.split.map(&:to_i)
-class User
-  @follow = []
+require 'set'
 
+n, q = gets.chomp.split.map(&:to_i)
+
+T = Array.new(q)
+A = Array.new(q)
+B = Array.new(q)
+q.times { |i|
+  T[i], A[i], B[i] = gets.chomp.split.map(&:to_i)
+}
+
+class User
   def initialize(id)
     @id = id
+    @follow_list = Set.new
   end
 
   def follow(id)
-    @follow.append(id)
+    @follow_list.add(id)
   end
 
   def unfollow(id)
-    @follow.delete(id)
+    @follow_list.delete(id)
+  end
+
+  def include_follow_list?(id)
+    @follow_list.include?(id)
   end
 end
 
 Users = []
 n.times { |i| Users.append(User.new(i)) }
 
-t = Array.new(n)
-a = Array.new(n)
-b = Array.new(n)
-
 q.times { |i|
-  t[i], a[i], b[i] = gets.chomp.split.map(&:to_i)
+  t, a, b = T[i], A[i] - 1, B[i] - 1
+
+  if t == 1
+    Users[a].follow(b)
+  elsif t == 2
+    Users[a].unfollow(b)
+  elsif t == 3
+    if Users[a].include_follow_list?(b) and Users[b].include_follow_list?(a)
+      puts "Yes"
+    else
+      puts "No"
+    end
+  end
 }
