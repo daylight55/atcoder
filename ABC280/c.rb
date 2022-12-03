39aecf4b8ecdcca6dbe6f4f5d0b0fe4889db5e9a
daylight55 <jojomasanori3@gmail.com>
2022-12-03 22:26:03 +0900

    abc280: improve c

--- ABC280/c.rb
+++ ABC280/c.rb
@@ -1,69 +1,16 @@
-S = gets.chomp
-T = gets.chomp
+S = gets.chomp.chars
+T = gets.chomp.chars
 
-s = S.chars.tally
-t = T.chars.tally
+s = S.tally
+t = T.tally
 diff = t.to_a - s.to_a
 
-# substr_list_s = S.scan(Regexp.new("#{diff[0][0]}+")).sort
-# substr_list_t = T.scan(Regexp.new("#{diff[0][0]}+")).sort
+s_diff_index_list = S.each_index.select{|i| S[i] == diff[0][0] }
+t_diff_index_list = T.each_index.select{|i| T[i] == diff[0][0] }
 
-# diff_substr = ""
-# substr_list_s.each_with_index{ | substr_s, i |
-
-#   p "#{substr_s} #{substr_list_t[i]}"
-#   if substr_s != substr_list_t[i]
-#     p "ok"
-#     diff_substr = substr_s
-#     break
-#   end
-# }
-
-def scan_str(str, pattern)
-  res = []
-  (0..str.length).each do |i|
-    res << [Regexp.last_match.to_s, i] if str[i..-1] =~ /^#{pattern}/
+t_diff_index_list.each_with_index{ |t_i, i|
+  if t_i != s_diff_index_list[i]
+    puts t_i + 1
+    break
   end
-  res
-end
-
-# puts T.index(diff_substr) + 1
-
-substr_list_s = scan_str(S, Regexp.new("#{diff[0][0]}")).sort
-substr_list_t = scan_str(T, Regexp.new("#{diff[0][0]}")).sort
-
-diff = substr_list_t - substr_list_s
-
-puts diff[0][1] + 1
-
-# pp substr_list_s
-# pp substr_list_t
-
-# substr_list_s.each_with_index{ | substr_s, i |
-
-#   p "#{substr_s} #{substr_list_t[i]}"
-#   if substr_s != substr_list_t[i]
-#     p "ok"
-#     diff_substr = substr_s
-#     break
-#   end
-# }
-
-
-
-#------------------------------------------------------#
-
-
-
-# puts T.find_index{ |t| t == diff[0][0] } + 1
-
-# S.each_with_index { |s, i|
-#   if s == T[i]
-#     next
-#   else
-#     diff_index = i
-#     break
-#   end
-# }
-
-# puts diff_index
+}
