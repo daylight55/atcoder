0d47f0b80c9f1ccfb5df20da5e611b048520b225
daylight55 <jojomasanori3@gmail.com>
2022-12-17 22:25:41 +0900

    abc282: solve c

--- ABC282/c.rb
+++ ABC282/c.rb
@@ -1,28 +1,18 @@
-class Array
-  # 特定の条件のインデックスを全て返す
-  def select_indice &p; map.with_index{|x, i| i if p.call(x)}.compact end
-end
-#---------------------------------
+N = gets.chomp.to_i
+S = gets.chomp
 
-# N = gets.chomp.to_i
-# S = gets.chomp
+in_quoto = false
+result = ""
 
-N = 20
-S = 'a,"t,"c,"o,"d,"e,"r,'
-
-A = S.split('"')
-
-# やり方1
-# "のインデックス一覧をとって、範囲に入ってなければ変換
-
-# quoto_indexes = []
-# quoto_indexes = S.select_indice{|x| x == '"'}
-# puts quoto_indexes.join(" ")
-
-
-# quoto_num = quoto_indexes.length
-
-R = Array.new(N)
-A.each_with_index{ |a, i|
-  A[i] == a.gsub() if i % 2 != 0
+S.each_char { |c|
+  if c == '"'
+    in_quoto = !in_quoto
+    result << c
+  elsif c == ','
+    result << (in_quoto ? ',' : '.')
+  else
+    result << c
+  end
 }
+
+puts result
