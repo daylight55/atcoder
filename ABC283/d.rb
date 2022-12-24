3c9bdf27e0862fe0d03f856c950117f3be81d955
daylight55 <jojomasanori3@gmail.com>
2022-12-24 22:06:15 +0900

    abc283 wip d

--- ABC283/d.rb
+++ ABC283/d.rb
@@ -1,22 +1,37 @@
-S = gets.chomp.chars
+S = gets.chomp
+# S = "(a(ba))"
 
-# 入れ子の括弧の中身をグループ化
-# その組みの中に重複する文字があればその時点でアウト
-# それ以外ならスルーして最後までチェックで終わり
+def good_string?(s)
+  # 箱を表す配列を用意する
+  box = []
 
-box = []
-# 一回愚直にやってみる
-S.each_with_index{ |s, i|
-  case s
+  # 文字列 s の 1 文字目から順に処理する
+  s.each_char.with_index do |c, i|
+    # c が英小文字の場合
+    if c =~ /[a-z]/
+      # 箱に c のボールがすでに入っている場合、気を失う
+      return false if box.include?(c)
 
-  when "(" then
-    next
-  when ")" then
-    # jを探索
-    delete_list = S.slice(i, j)
-    box = box - delete_list
-  else
-    box << s
+      # 箱に c のボールを入れる
+      box << c
+    # c が ) の場合
+    elsif c == ")"
+      # ii 未満の整数 jj を探す
+      jj = i - 1
+      while jj >= 0
+        # SS の jj 番目から ii 番目までの文字からなる文字列が良い文字列となる最大の整数 jj を取る
+        break if s[jj] == "("
+        jj -= 1
+      end
+
+      # jj 番目から ii 番目までの操作で箱に入れたボールをすべて、箱から取り出す
+      box.pop(i - jj)
+    end
   end
 
-}
+  # 最終的に箱が空になっていれば、高橋君が気を失わずに一連の操作を完了させられる
+  box.empty?
+end
+
+result = good_string?(S) ? "Yes" : "No"
+puts result
