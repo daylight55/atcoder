84c166ebc6ff78ad5110c16132fd4a56b73257e2
daylight55 <jojomasanori3@gmail.com>
2023-01-15 21:42:58 +0900

    abc285: tle b

---
+++ ABC285/b.rb
@@ -0,0 +1,18 @@
+N = gets.chomp.to_i
+S = gets.chomp
+
+dp = Array.new(N) { }
+
+l = 0
+for i in 1..N-1 do
+  for j in 0..N-i-1 do
+    if S[j] != S[j + i]
+      l += 1
+    else
+      break
+    end
+  end
+  # puts "i:#{i} j:#{j} l:#{l}"
+  puts l
+  l = 0
+end
