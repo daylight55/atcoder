ab957e932e83eb5fbce4751e4f35d930d469d4dc
daylight55 <jojomasanori3@gmail.com>
2023-01-15 23:31:52 +0900

    abc285: solve b

--- ABC285/b.rb
+++ ABC285/b.rb
@@ -1,19 +1,16 @@
-# N = gets.chomp.to_i
-# S = gets.chomp
-N = 6
-S = "abcbac"
+N = gets.chomp.to_i
+S = gets.chomp.chars
 
 l = 0
 for i in 1..N-1 do
   for j in 0..N-i-1 do
-    puts "i:#{i} j:#{j} l:#{l}"
     if S[j] != S[j + i]
       l += 1
     else
       break
     end
   end
-
+  # puts "i:#{i} j:#{j} l:#{l}"
   puts l
   l = 0
 end
