d0150c385c866e81de0c5a55b446114100411031
daylight55 <jojomasanori3@gmail.com>
2023-02-11 22:16:24 +0900

    abc289: wip b

--- ABC289/b.rb
+++ ABC289/b.rb
@@ -1,47 +1,31 @@
-# ref. https://qiita.com/k_karen/items/5349a25c3eb7b4697f58
-class UnionFind
-  attr_accessor :parent
-
-  def initialize(size)
-    @rank = Array.new(size, -1)
-    @parent = Array.new(size, &:itself)
-  end
-
-  def merge(id_x, id_y)
-    x_parent = get_parent(id_x)
-    y_parent = get_parent(id_y)
-    return if x_parent == y_parent
-
-    if @rank[x_parent] > @rank[y_parent]
-      @parent[y_parent] = x_parent
-    else
-      @parent[x_parent] = y_parent
-      @rank[y_parent] += 1 if @rank[x_parent] == @rank[y_parent]
-    end
-  end
-
-  def get_parent(id_x)
-    @parent[id_x] == id_x ? id_x : (@parent[id_x] = get_parent(@parent[id_x]))
-  end
-
-  def same_parent?(id_x, id_y)
-    get_parent(id_x) == get_parent(id_y)
-  end
-
-  def size
-    @parent.map { |id_x| get_parent(id_x) }.uniq.size
-  end
-end
-#---------------------------------------
-
 N, M = gets.chomp.split.map(&:to_i)
 a = gets.chomp.split.map(&:to_i)
 
-uf = UnionFind.new(N)
-pp uf.parent
-
-a.each { |re_num|
-  uf.merge(re_num, re_num + 1)
+message = ''
+re_index = 0
+(1..N).each_with_index { |n, i|
+  if n == a[re_index]
+    message = message + n.to_s
+    message = message + 'レ'
+    re_index += 1
+  else
+    message = message + n.to_s
+  end
 }
 
-pp uf
+message_groups = message.split(/レ/)
+# puts message.split(/レ/)
+
+ans = Array.new(message_groups.length, Array.new())
+next_group = []
+message_groups.length.times{ |i|
+  # 1個ならレ点で繋がっているため素直に追加
+  if message_groups[i].length == 1
+    ans << message_groups[i]
+  # 複数個であればレ点の終わりを示す
+  else
+    # 初めは繋がっていたため追加
+    ans << message_groups[i].first
+    next_group
+  end
+}
