c0b5c19336b1fd01aed0007e44fa9fda24537f3f
daylight55 <jojomasanori3@gmail.com>
2023-02-12 22:42:54 +0900

    abc289: solve c

--- ABC289/c.rb
+++ ABC289/c.rb
@@ -1,8 +1,24 @@
-N, M = gets.chomp.split.map(&:to_i)
+require 'set'
 
-C = Array.new(M)
-a = Array.new(M)
-M.times {
-  C << gets.chomp.to_i
-  a << gets.chomp.split.map(&:to_i)
+N, M = gets.split.map(&:to_i)
+a = []
+M.times{
+  c = gets.chomp.to_i
+  a << gets.split.map(&:to_i)
 }
+
+ans = 0
+# bit全探索により、選択方法の全ての選び方を対応取る
+# bitの各桁がS1,S2,...,Snの集合の選択に対応する。
+# 1が立つ場合に選択、0の場合に未選択として扱う。
+(1 << M).times { |b|
+  # 選択した集合を記録する
+  t = Set.new
+  M.times { |i|
+    # 2進数表記で0の場合、つまり未選択の場合集合に追加する
+    a[i].each { |x| t.add(x) } if b[i].zero?
+  }
+  # 集合の大きさがNに一致する時、つまり1以上N以下の全てのxを含む時にカウントする
+  ans += 1 if t.size == N
+}
+puts ans
