be46afe8a043a2b303a92c527f0fb4a3956583db
daylight55 <jojomasanori3@gmail.com>
2023-02-19 22:27:36 +0900

    abc290: wip c

--- ABC290/c.rb
+++ ABC290/c.rb
@@ -1,28 +1,23 @@
+require 'set'
+
 n, k = gets.chomp.split.map(&:to_i)
-A = gets.chomp.split.map(&:to_i)
+A = gets.chomp.split.map(&:to_i).sort
 
-def mex(set)
-  m = -1
-  # 0を含んでいない場合、0を返す
-  return 0 if !array.include?(0)
-
-  # 0を含んでいる場合、順番に探す
-  (array.max + 1).times{ |i|
-    if !array.include?(i)
-      m = i
-      break
-    end
-  }
-  # 更新できなかった場合は最大値と一致しているため最大値 + 1を返す
-  m = array.max + 1 if m == -1
-  return m
-end
+# def mex(array)
+#   return 0 unless array.include?(0)
+#   max = array.max
+#   set = array.to_set
+#   (0..max+1).each do |i|
+#     return i unless set.include?(i)
+#   end
+# end
 
 mex_list = []
-A.combination(k).to_a.uniq.each{ |c|
-  c_set = c.to_set
-  m = mex(c_set)
-  # puts "c_set: #{c_set.join(' ')} \n m: #{m}"
+
+(0..n-k).each{ |i|
+  c = A[i, k]
+  m = (0..k).find { |j| !s.include?(j) }
+  # puts "c: #{c.join(' ')} m: #{m}"
   mex_list << m
 }
 
