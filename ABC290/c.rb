5bf44cd459f68e2352458934720cd446c3a04255
daylight55 <jojomasanori3@gmail.com>
2023-02-22 23:09:47 +0900

    abc290: solve c

--- ABC290/c.rb
+++ ABC290/c.rb
@@ -1,24 +1,12 @@
 require 'set'
 
 n, k = gets.chomp.split.map(&:to_i)
-A = gets.chomp.split.map(&:to_i).sort
+a = gets.chomp.split.map(&:to_i).to_set
 
-# def mex(array)
-#   return 0 unless array.include?(0)
-#   max = array.max
-#   set = array.to_set
-#   (0..max+1).each do |i|
-#     return i unless set.include?(i)
-#   end
-# end
-
-mex_list = []
-
-(0..n-k).each{ |i|
-  c = A[i, k]
-  m = (0..k).find { |j| !s.include?(j) }
-  # puts "c: #{c.join(' ')} m: #{m}"
-  mex_list << m
-}
-
-puts mex_list.max
+k.times do |i|
+  if !a.include?(i)
+    puts i
+    exit
+  end
+end
+puts k
