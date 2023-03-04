802582da8768138b7b24043092bba1e5e158c266
daylight55 <jojomasanori3@gmail.com>
2023-03-04 22:27:54 +0900

    wip c

--- abc292/c/main.rb
+++ abc292/c/main.rb
@@ -22,7 +22,7 @@ prime = Prime.instance
 n = gets.chomp.to_i
 count = 0
 
-(1..Math.square(n)).each{ |i|
+(1..n-1).each{ |i|
   # ABとCDの組み合わせは対応するから半分だけ求めたら倍にすればいい。
   # Nを表現できる和の組み合わせを考える
   # その後、各数字を素因数分解し、その素因数の組み合わせを考える
@@ -30,17 +30,43 @@ count = 0
   # まずABを決める
   ab = i
   # abの約数の数を求める
-  ab_divisor_size = prime.each_divisor(ab).size
+  # ab_divisor_size = prime.each_divisor(i).size
   # puts "i:#{i} ab_factors:#{ab_divisor_size}"
 
-  # その後、CDを決める
+  # それぞれの数を求める
   cd = n - i
+
+  prime.each_divisor(cd) { |c|
+    puts "ab: #{ab} cd: #{cd} c:#{c}"
+
+    d = cd / c
+    a = ab / d
+    puts "a:#{a} b:#{b} c:#{c} d:#{d}"
+    b = ab / a
+
+    if a == b and c == d
+      # AB,CDを入れ替えた2通り
+      count += 2
+    elsif a == b or c != d
+      # AB固定のC,D入れ替え、またその逆の4通り
+      count += 4
+    elsif a != b and c == d
+      # 直上と同様
+      count += 4
+    elsif a!= b and c != d
+      # 全て入れ替え可能なため8通り
+      count += 8
+    end
+
+
+  }
+
   # cdの約数の数を求める
-  cd_divisor_size = prime.each_divisor(cd).size
+  # cd_divisor_size = prime.each_divisor(cd).size
   # puts "i:#{i} cd_factors:#{cd_divisor_size}"
 
   # 個数を掛け算する
-  count += ab_divisor_size * cd_divisor_size
+  # count += ab_divisor_size * cd_divisor_size
   # puts "count #{count}"
 }
 
