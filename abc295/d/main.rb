78ab9d8760a5c3f5c00a8e1ed8d312bbbe667463
daylight55 <jojomasanori3@gmail.com>
2023-04-09 20:06:41 +0900

    abc295: wip d

--- abc295/d/main.rb
+++ abc295/d/main.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
-N, Q = gets.chomp.split.map(&:to_i)
-events = Array.new(Q){ gets.chomp.split.map(&:to_i) }
+S = gets.chomp.chars
+ans = 0
+S.tally.each{ |k, v|
+  # 各文字の出現回数のカウントから文字を並び替えることによって同じ列を2度繰り返すようにできる文字列の個数を求める
+  # 例えば、'aabb'の場合、'abab'と'aabb'の2通り
+  # この場合、'aabb'の文字列の個数は、'a'が2個、'b'が2個あるので、2 * 2 = 4通り
+  def combination(n, r)
+    return 1 if r == 0
+    return n if r == 1
+    return combination(n, n - r) if r > n / 2
+    (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+  end
+  ans += combination(v, 2)
+}
+
+puts ans
