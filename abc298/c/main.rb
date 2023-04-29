a9ccfc0b6e1f30ba8efef2ba02acdead889e9f71
daylight55 <jojomasanori3@gmail.com>
2023-04-29 20:51:23 +0900

    abc298: wip

--- abc298/c/main.rb
+++ abc298/c/main.rb
@@ -12,18 +12,35 @@ query.each do |q|
   if event == 1
     i = q[1]
     j = q[2]
-    box[j] = [] if box[j].nil?
-    box[j] << i
+    if box[j].nil?
+      box[j] = {}
+      # 箱jに数iを入れる
+      box[j][i] = 1
+      next
+    else
+      if box[j][i].nil?
+        # 箱jに数iを入れる
+        box[j][i] = 1
+      else
+        # 箱jに数iを追加
+        box[j][i] += 1
+      end
+    end
   elsif event == 2
     i = q[1]
-    # 箱iに書かれている数をすべて出力する
-    puts box[i].sort.join(" ")
+    # 箱iに書かれているiを個数分出力して数をすべて昇順で出力する
+    result = []
+    box[i].each do |k, v|
+      # puts "k: #{k}, v: #{v}"
+      result << [k] * v
+    end
+    puts result.sort.join(" ")
   elsif event == 3
     i = q[1]
     # 数iを含む箱の番号をすべて昇順で出力する
     result = []
     box.each do |k, v|
-      result << k if v.include?(i)
+      result << k if !v[i].nil?
     end
     puts result.sort.join(" ")
   end
