7a866ebee828129e3f16c8ecdc852033e30eee43
daylight55 <jojomasanori3@gmail.com>
2023-05-13 22:00:18 +0900

    abc301: solve b

--- abc301/b/main.rb
+++ abc301/b/main.rb
@@ -1,16 +1,38 @@
 #!/usr/bin/env ruby
 n = gets.chomp.to_i
 a = gets.chomp.split.map(&:to_i)
-a_copy = a.dup
+insert_targets = []
 
 n.times do |i|
-  # 隣接する要素の差が2以上ならば、隣接する要素の間を埋める順列を挿入する
-  if (a[i] - a[i + 1]).abs >= 2 && i < n - 1
-    nums = [*(a[i] + 1)..(a[i + 1] - 1)]
+  # 隣接する要素の差が2以上のインデックスのペアを記録する
+  if i < n - 1
+    if (a[i] - a[i + 1]).abs >= 2
+      insert_targets << [i, i + 1]
+    end
+  end
+end
 
-    # 挿入する箇所を決める
-    a_copy.insert((i + 1), nums)
-    puts "i: #{i} nums: #{nums} a_copy: #{a_copy.flatten.join(" ")}"
+# pp insert_targets
+
+insert_targets.each do |target|
+  # その数字の間を埋める連番を挿入する
+  # 左が大きい時
+  if a[target[0]] > a[target[1]]
+    nums = (a[target[1]] + 1..a[target[0]] - 1).to_a.reverse
+    a.insert(target[1], nums).flatten!
+  # 右が大きい時
+  else
+    nums = (a[target[0]] + 1..a[target[1]] - 1).to_a
+    a.insert(target[1], nums).flatten!
+  end
+
+  # 今回追加した対象以降のターゲットのインデックスを更新する
+  insert_targets.map! do |t|
+    if t[0] > target[0]
+      [t[0] + nums.length, t[1] + nums.length]
+    else
+      t
+    end
   end
 end
 
