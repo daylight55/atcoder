ce42efcbb2da7473cfcb33d588e8532daba62f5e
daylight55 <jojomasanori3@gmail.com>
2023-05-27 20:59:43 +0900

    wip: abc301

--- abc301/c/main.rb
+++ abc301/c/main.rb
@@ -1,27 +1,59 @@
 #!/usr/bin/env ruby
 s = gets.chomp.chars
 t = gets.chomp.chars
+atcoder = "atcoder".chars
 
 # @をワイルドカードとして、イカサマによって順番は無視できるためカードの要素数が同じになることを確認する
 s_tally = s.tally
 t_tally = t.tally
 
-# s,tそれぞれのtallyの各要素をキーに要素数の合計を記録する
-all_tally = s_tally.merge(t_tally) { |key, s_value, t_value| s_value + t_value }
+# @の数をカウントする
+s_at_count = s_tally['@'] || 0
+t_at_count = t_tally['@'] || 0
+s_tally.delete('@')
+t_tally.delete('@')
 
-# @の数を数える
-at_count = all_tally["@"] || 0
-all_tally.delete("@")
+# s,tそれぞれのtallyのキーをマージして配列にする
+all_keys = s_tally.keys | t_tally.keys
 
-# 各キーの要素数が偶数であることを確認する。奇数の場合、@を使って補えるかどうかを確認する
-all_tally.each do |key, value|
-  # 奇数の場合のみチェック
-  if value.odd?
-    # @を使って補えるかどうかを確認する
-    if at_count >= value - 1
-      at_count -= value - 1
-      next
-    # @を使って補えない場合終了
+# 各要素の数が同じかどうかを確認する
+all_keys.each do |key|
+  value = s_tally[key] || t_tally[key]
+
+  # キーの組み合わせを確認する。両方に存在
+  if s_tally[key] && t_tally[key] && key in atcoder
+    # 両方に存在する場合はどちらが多いかを確認する
+    # tが多い場合
+    if s_tally[key] > t_tally[key]
+      if t_tally[key] + t_at_count >= s_tally[key]
+        t_at_count -= s_tally[key] - t_tally[key]
+      else
+        puts "No"
+        exit
+      end
+    # sが多い場合
+    elsif s_tally[key] < t_tally[key]
+      if s_tally[key] + s_at_count >= t_tally[key]
+        s_at_count -= t_tally[key] - s_tally[key]
+      else
+        puts "No"
+        exit
+      end
+    end
+  # sのみに存在
+  elsif s_tally[key] && !t_tally[key]
+    # tが@で補える場合は一致する
+    if s_tally[key] <= t_at_count
+      t_at_count -= s_tally[key]
+    else
+      puts "No"
+      exit
+    end
+  # tのみに存在
+  elsif !s_tally[key] && t_tally[key]
+    # sが@で補える場合は一致する
+    if t_tally[key] <= s_at_count
+      s_at_count -= t_tally[key]
     else
       puts "No"
       exit
@@ -30,45 +62,3 @@ all_tally.each do |key, value|
 end
 
 puts "Yes"
-
-# # @の数を数える
-# s_at_count = s_tally["@"] || 0
-# t_at_count = t_tally["@"] || 0
-# s_tally.delete("@")
-# t_tally.delete("@")
-
-# # 各要素の数が同じかどうかを確認する
-# s_tally.each do |key, value|
-#   puts "key: #{key}, value: #{value}"
-#   puts "t_tally[key]: #{t_tally[key]} t_at_count: #{t_at_count}"
-#   puts "s_at_count: #{s_at_count}"
-
-#   if t_tally[key].nil?
-#     puts "No"
-#     exit
-#   end
-#   if t_tally[key] < value
-#     # @を使って補えるかどうかを確認する
-#     if t_at_count >= value - t_tally[key]
-#       t_at_count -= value - t_tally[key]
-#       next
-#     else
-#       puts "No"
-#       exit
-#     end
-#   elsif t_tally[key] > value
-#     # @を使って補えるかどうかを確認する
-#     if s_at_count >= t_tally[key] - value
-#       s_at_count -= t_tally[key] - value
-#       next
-#     else
-#       puts "No"
-#       exit
-#     end
-#   # 元から同じなら続行
-#   elsif t_tally[key] == value
-#     next
-#   end
-# end
-
-# puts "Yes"
