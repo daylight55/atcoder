b84a182c961b81842b695f2b9bd03f83f1a483e0
daylight55 <jojomasanori3@gmail.com>
2023-05-27 22:32:37 +0900

    abc303: wip d

--- abc303/d/main.rb
+++ abc303/d/main.rb
@@ -1,2 +1,50 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split
+X, Y, Z = gets.chomp.split.map(&:to_i)
+S = gets.chomp.chars
+
+capslock = false
+sum = 0
+# 連続する文字をグループ化する
+grouping = S.slice_when { |a, b| a != b }.to_a
+# pp grouping
+
+grouping.each do |group|
+  n = group.length
+  # NOTE: CapsLockの判断は次のグループの文字によっても決まる？
+  # Aの場合
+  if group[0] == 'A'
+    # CapsLockがOFFの場合
+    if capslock == false
+      # Capslockを押した方が早い場合
+      if n*Y > Z+X*n
+        capslock = true
+        sum += Z+X*n
+      # そのままが早い場合
+      else
+        sum += n*Y
+      end
+    # CapsLockがONの場合
+    else
+      # 順当にaを押せばいい
+      sum += n*X
+    end
+  # aの場合
+  else
+    # CapsLockがOFFの場合
+    if capslock == false
+      # 順当にaを押せばいい
+      sum += n*X
+    # CapsLockがONの場合
+    else
+      # Capslockを押した方が早い場合
+      if n*Y > Z+X*n
+        capslock = false
+        sum += Z+X*n
+      else
+        sum += n*Y
+      end
+    end
+  end
+end
+
+puts sum
