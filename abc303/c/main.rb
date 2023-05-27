685c3665ea133291c32d0d8e70d759759f28ea17
daylight55 <jojomasanori3@gmail.com>
2023-05-27 22:01:29 +0900

    abc303: solve c

--- abc303/c/main.rb
+++ abc303/c/main.rb
@@ -3,23 +3,16 @@ require 'set'
 
 N, M, H, K = gets.chomp.split.map(&:to_i)
 MOVES = gets.chomp.chars
-ITEMS = Set.new
+items = Set.new
 M.times do
-  ITEMS << gets.chomp.split.map(&:to_i)
+  items << gets.chomp.split.map(&:to_i)
 end
 
 pos = [0, 0]
 health = H
 
 MOVES.each do |move|
-  # pp "pos: #{pos}, health: #{health} move: #{move}"
-
-  # Kが負になったら終了
-  if health <= 0
-    puts 'No'
-    exit
-  end
-
+  # pp "before >> pos: #{pos}, health: #{health} move: #{move}"
   case move
   when 'R'
     # 右に1マス
@@ -39,10 +32,19 @@ MOVES.each do |move|
     health -= 1
   end
 
-  # アイテムがある場合は拾う
-  if ITEMS.include?(pos) && health < K
-    health = K
+  # Kが負になったら終了
+  if health < 0
+    puts 'No'
+    exit
   end
+
+  # アイテムがある場合は拾う
+  if items.include?(pos) && health < K
+    health = K
+    items.delete(pos)
+  end
+
+  # pp "after  >> pos: #{pos}, health: #{health}"
 end
 
 # ループを抜けたら移動完了
