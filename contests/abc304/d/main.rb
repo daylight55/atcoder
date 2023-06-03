b1d7b7bf7c65e5f321e91c85ce0d32f2fcd7dedc
daylight55 <jojomasanori3@gmail.com>
2023-06-03 23:49:08 +0900

    abc304: solve d

--- contests/abc304/d/main.rb
+++ contests/abc304/d/main.rb
@@ -7,30 +7,18 @@ CUTS_X = gets.chomp.split.map(&:to_i)
 B = gets.chomp.to_i
 CUTS_Y = gets.chomp.split.map(&:to_i)
 
-# STRAWBERRIES: イチゴの座標 (x, y)
-# A: 横方向のカットの数
-# CUTS_X: 横方向のカットの座標 x
-# B: 縦方向のカットの数
-# CUTS_Y: 縦方向のカットの座標 y
-
 # カット後のピースに含まれるイチゴの数を記録
-strawberry_blocks = {}
+strawberry_blocks = Hash.new(0)
 
-CUTS_X.each do |cut_x|
-  CUTS_Y.each do |cut_y|
-    # カット後のピースの左下の座標を計算
-    x = cut_x
-    y = cut_y
-
-    # カット後のピースに含まれるイチゴの数を計算
-    strawberry_blocks[[x, y]] = 0
-    STRAWBERRIES.each do |strawberry|
-      # カットの領域にあるかどうかを判定
-      
-
-    end
-  end
+STRAWBERRIES.each do |strawberry_x, strawberry_y|
+  # イチゴの上界にあたるラインを取得する
+  x_blocK_line = CUTS_X.bsearch { |cut_x| cut_x > strawberry_x } || W
+  y_blocK_line = CUTS_Y.bsearch { |cut_y| cut_y > strawberry_y } || H
+  strawberry_blocks[[x_blocK_line, y_blocK_line]] += 1
 end
 
+# カウントされたイチゴのブロック数が、カットで作られる総ブロック数より少ない場合は、イチゴを含まないブロックが存在するため最小値は0
+min = strawberry_blocks.size < (A + 1) * (B + 1) ? 0 : strawberry_blocks.values.min
+max = strawberry_blocks.values.max
 # ストロベリーブロックの最大値と最小値を出力
-puts "#{strawberry_blocks.values.min} #{strawberry_blocks.values.max}"
+puts "#{min} #{max}"
