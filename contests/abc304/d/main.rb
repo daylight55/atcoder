e704f83ac2dd98c5c95725c4176814d630de9308
daylight55 <jojomasanori3@gmail.com>
2023-06-03 22:48:16 +0900

    abc304: wip d

--- contests/abc304/d/main.rb
+++ contests/abc304/d/main.rb
@@ -3,27 +3,34 @@ W, H = gets.chomp.split.map(&:to_i)
 N = gets.chomp.to_i
 STRAWBERRIES = Array.new(N) { gets.chomp.split.map(&:to_i) }
 A = gets.chomp.to_i
-CUTS_HORIZONTAL = gets.chomp.split.map(&:to_i)
+CUTS_X = gets.chomp.split.map(&:to_i)
 B = gets.chomp.to_i
-CUTS_VERTICAL = gets.chomp.split.map(&:to_i)
+CUTS_Y = gets.chomp.split.map(&:to_i)
 
-def cut_strawberries(strawberries, cuts)
-  cuts.each do |cut|
-    strawberries = strawberries.map do |strawberry|
-      if strawberry[0] < cut
-        [strawberry[0], strawberry[1], cut, strawberry[3]]
-      else
-        [cut, strawberry[1], strawberry[2], strawberry[3]]
-      end
+# STRAWBERRIES: イチゴの座標 (x, y)
+# A: 横方向のカットの数
+# CUTS_X: 横方向のカットの座標 x
+# B: 縦方向のカットの数
+# CUTS_Y: 縦方向のカットの座標 y
+
+# カット後のピースに含まれるイチゴの数を記録
+strawberry_blocks = {}
+
+CUTS_X.each do |cut_x|
+  CUTS_Y.each do |cut_y|
+    # カット後のピースの左下の座標を計算
+    x = cut_x
+    y = cut_y
+
+    # カット後のピースに含まれるイチゴの数を計算
+    strawberry_blocks[[x, y]] = 0
+    STRAWBERRIES.each do |strawberry|
+      # カットの領域にあるかどうかを判定
+      
+
     end
   end
-  strawberries
 end
 
-def count_strawberries(strawberries)
-  strawberries.map { |strawberry| (strawberry[2] - strawberry[0]) * (strawberry[3] - strawberry[1]) }.inject(:+)
-end
-
-strawberries = cut_strawberries(STRAWBERRIES, CUTS_HORIZONTAL)
-strawberries = cut_strawberries(strawberries, CUTS_VERTICAL)
-puts count_strawberries(strawberries)
+# ストロベリーブロックの最大値と最小値を出力
+puts "#{strawberry_blocks.values.min} #{strawberry_blocks.values.max}"
