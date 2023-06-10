36dcd421e1e1edce73c0914f53c187c8b05ebb14
daylight55 <jojomasanori3@gmail.com>
2023-06-10 22:22:12 +0900

    abc305: solve c

--- contests/abc305/c/main.rb
+++ contests/abc305/c/main.rb
@@ -3,30 +3,36 @@ H, W = gets.chomp.split.map(&:to_i)
 GRID = Array.new(H) { gets.chomp.chars }
 
 ans = []
-
+# クッキーの範囲を求める
+cookies_frame = Array.new(4, nil)
 GRID.each_with_index do |row, i|
   row.each_with_index do |cell, j|
     if cell == '#'
-      # 自分の右に'#'があれば継続
-      next if GRID[i][j + 1] == '#'
-
-      # puts "i: #{i}, j: #{j}"
-      # puts "GRID[i][j + 1]: #{GRID[i][j + 1]} GRID[i + 1][j]: #{GRID[i + 1][j]}"
-
-      # 自分の右が穴抜けかつ、自分の下に'#'がある
-      if GRID[i][j + 1] == '.' && GRID[i + 1][j] == '#'
-        # 左が# (穴抜け後に読んだ右端が誤読)
-        if j - 1 >= 0 && GRID[i][j - 1] == '#'
-          if i - 1 >= 0 && GRID[i - 1][j] == '#'
-            ans = [i, j + 1]
-            break
-          end
-        end
-        ans = [i, j + 1]
-        break
+      if cookies_frame[0] == nil
+        cookies_frame[0] = i
+        cookies_frame[1] = j
+        cookies_frame[2] = j
+        cookies_frame[3] = j
+      else
+        cookies_frame[0] = [cookies_frame[0], i].min
+        cookies_frame[1] = [cookies_frame[1], i].max
+        cookies_frame[2] = [cookies_frame[2], j].min
+        cookies_frame[3] = [cookies_frame[3], j].max
       end
     end
   end
 end
 
-puts ans.map{|i| i + 1}.join(' ')
+# puts cookies_frame.join(' ')
+
+# クッキーの範囲に.のマスがあれば出力
+GRID.each_with_index do |row, i|
+  row.each_with_index do |cell, j|
+    if cookies_frame[0] <= i && i <= cookies_frame[1] && cookies_frame[2] <= j && j <= cookies_frame[3]
+      if cell == '.'
+        puts "#{i + 1} #{j + 1}"
+        exit
+      end
+    end
+  end
+end
