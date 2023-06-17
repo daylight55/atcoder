8d4d6fd5feb1ec147c6fd4d53fd77b7275c240a1
daylight55 <jojomasanori3@gmail.com>
2023-06-17 23:44:15 +0900

    wip

--- contests/abc306/d/main.rb
+++ contests/abc306/d/main.rb
@@ -2,21 +2,28 @@
 N = gets.chomp.to_i
 MENU = Array.new(N) { gets.chomp.split.map(&:to_i) }
 
-# 高橋君のお腹の状態を個別にもつ
-# dp[i][0]: お腹を壊している時の美味しさの最大値
-# dp[i][1]: お腹を壊していない時の美味しさの最大値
-dp = Array.new(N) { Array.new(2, Float::INFINITY) }
+# dp[i][0] : お腹が正常
+# dp[i][1] : お腹を壊している
+dp = Array.new(N + 1) { Array.new(2, -Float::INFINITY) }
+# 初期値はお腹が正常で美味しさは0
+dp[0][0] = 0
 
-# 高橋君はメニューを食べるか食べないかを選べる
-# yが美味しさで、美味しさの合計を最大化したい
 MENU.each.with_index do |(x, y), i|
-  # x = 0: 毒入りでこの時お腹を壊す。またすでにお腹を壊している場合は死ぬ。
-  # x = 1: 解毒剤入りでことの時お腹を壊していた場合は治る。お腹を壊していない場合は何も起こらない。
-  stomachache = false
-
+  # 食べる場合
+  # NOTE: 毒があるかで死ぬかの瀬戸際なので遷移の条件が変わる
+  # 毒がない場合
   if x == 0
-    stomachache = true
-    dp[i][0] = [dp[i][0], y].max
-  elsif x == 1 && dp[i][0] == Float::INFINITY
-    stomachache = false
+    # 正常、腹痛それぞれから今回のメニューを食べることができる
+    dp[i + 1][0] = [dp[i][0], dp[i][0] + y].max
+  # 毒入りの場合
+  else
+    # 正常からのみ今回のメニューを食べることができる
+    dp[i + 1][1] = [dp[i][1], dp[i][0] + y].max
   end
+
+  # 食べない場合もあるので、食べた場合と前の状態を比較して高い方を記録
+  dp[i + 1][0] = [dp[i + 1][0], dp[i][0]].max
+  dp[i + 1][1] = [dp[i + 1][1], dp[i][1]].max
+end
+
+puts dp[N].max
