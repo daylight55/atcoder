0d23271cfc843e297ba21a322810d1714d49fc1b
daylight55 <jojomasanori3@gmail.com>
2023-07-15 21:27:22 +0900

    wip

--- acc_templates/ruby/main.rb
+++ acc_templates/ruby/main.rb
@@ -1,7 +1,26 @@
 #!/usr/bin/env ruby
 def main
-  N = gets.chomp.to_i
+  n, m = gets.chomp.split.map(&:to_i)
+  input = gets.chomp.split.map(&:to_i)
 
+  result = false
+  n.times do |i|
+    p, c = input[0], input[1]
+    f = input[2..-1]
+    # 価格がtarget以下の製品
+    condition = input.select{|x| x[0] <= p}
+    # targetが持つ機能数よりも多い機能を持つ製品
+    condition = condition.select{|x| x[1] >= c}
+    # 価格が未満であるか
+    condition_price = condition.select{|x| x[0] < p}
+    # 価格が未満であるか、i番目の製品にない機能を1つ以上持つ製品
+    condition_func = condition.select{|x| x[2..-1].any?{|y| !f.include?(y)}}
+
+    # 最後の条件のいずれかが存在していたらtrue
+    result = true if !condition_price.nil? || !condition_func.nil?
+  end
+
+  puts result ? 'Yes' : 'No'
 end
 
 # ---------------------------------------------------
