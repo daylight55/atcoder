39661bcd5341ec7901ed33f6cbf31532d2e4f86b
daylight55 <jojomasanori3@gmail.com>
2023-07-15 13:10:28 +0900

    abc308: wip d

--- contests/abc308/d/main.rb
+++ contests/abc308/d/main.rb
@@ -2,15 +2,23 @@
 H, W = gets.chomp.split.map(&:to_i)
 S = Array.new(H) { gets.chomp.chars }
 
+# スタート地点がsでない場合は論外
 if S[0][0] != 's'
   puts 'No'
   exit
 end
 
-queue = [[0, 0, 1]]
-visited = Array.new(H) { Array.new(W, false) }
 keyword = 'snuke'.chars
+keyword_indices = {}
+keyword.each_with_index do |char, index|
+  keyword_indices[char] = index
+end
 
+visited = Array.new(H) { Array.new(W, false) } # 探索済みのマスを管理する配列
+queue = [[0, 0, 1]] # キューには現在地の座標と次に探索するキーワードのインデックスを入れる
+visited[0][0] = true
+
+# キューが空になるまで幅優先探索
 while !queue.empty?
   i, j, index = queue.shift
   visited[i][j] = true
@@ -25,11 +33,13 @@ while !queue.empty?
   [[i - 1, j], [i + 1, j], [i , j - 1], [i, j + 1]].each do |ny, nx|
     # グリッドの範囲外や壁の場合は探索しない
     next if ny < 0 || ny >= H || nx < 0 || nx >= W
-    # 探索済みの場合は探索しない
-    next if visited[ny][nx]
+    next if visited[ny][nx] # 探索済みの場合は探索しない
+    next if !keyword.include?(S[ny][nx]) # キーワードが含まれていない場合は探索しない
 
+    # 順番にキーワードを探索していく
     next_index = (index % 5) + 1
     next_keyword = keyword[next_index - 1]
+    # キーワードが見つかったらキューに追加
     queue.push([ny, nx, next_index]) if S[ny][nx] == next_keyword
   end
 
