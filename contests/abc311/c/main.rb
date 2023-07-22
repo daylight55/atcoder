5578f145cad95dab3c4072bc5308a2d63a6a5176
daylight55 <jojomasanori3@gmail.com>
2023-07-22 22:09:22 +0900

    wip dfs

--- contests/abc311/c/main.rb
+++ contests/abc311/c/main.rb
@@ -8,91 +8,33 @@ def main
     graph[i] = a[i - 1]
   }
 
-  uf = UnionFind.new(n + 1)
-
+  visited = Array.new(n, false)
+  stack = []
   cycle = []
-  (0..n-1).each{|i|
-    if uf.same?(i, graph[i])
-      cycle_start = i
-      cycle_end = graph[i]
-      break
-    else
-      uf.merge(i, graph[i])
-    end
-  }
 
-  unless cycle_start.nil?
-    current = cycle_start
-    while current != cycle_end
-      cycle << current
-      current = graph[current]
+  (0..n-1).each{|start|
+    next if visited[start]
+    stack.push([start, [start]])
+
+    while !stack.empty?
+      v, path = stack.pop
+      visited[v] = true
+      next_v = graph[v]
+
+      if visited[v]
+        cycle_start = path.index(next_v)
+        cycle = path[cycle_start..-1]
+        break
+      else
+        stack.push([next_v, path + [next_v]])
+      end
     end
-  end
+
+    break if !cycle.empty?
+  }
 
   puts cycle.size
   puts cycle.join(" ")
-
 end
-
-# ---------------------------------------------------
-# ref. https://github.com/universato/ac-library-rb/blob/main/lib/dsu.rb
-class DSU
-  def initialize(n = 0)
-    @n = n
-    @parent_or_size = Array.new(n, -1)
-    # root node: -1 * component size
-    # otherwise: parent
-  end
-
-  attr_reader :parent_or_size, :n
-
-  def merge(a, b)
-    x = leader(a)
-    y = leader(b)
-    return x if x == y
-
-    x, y = y, x if -@parent_or_size[x] < -@parent_or_size[y]
-    @parent_or_size[x] += @parent_or_size[y]
-    @parent_or_size[y] = x
-  end
-  alias unite merge
-
-  def same?(a, b)
-    leader(a) == leader(b)
-  end
-  alias same same?
-
-  def leader(a)
-    unless 0 <= a && a < @n
-      raise ArgumentError.new, "#{a} is out of range (0...#{@n})"
-    end
-
-    @parent_or_size[a] < 0 ? a : (@parent_or_size[a] = leader(@parent_or_size[a]))
-  end
-  alias root leader
-  alias find leader
-
-  def [](a)
-    if @n <= a
-      @parent_or_size.concat([-1] * (a - @n + 1))
-      @n = @parent_or_size.size
-    end
-
-    @parent_or_size[a] < 0 ? a : (@parent_or_size[a] = self[@parent_or_size[a]])
-  end
-
-  def size(a)
-    -@parent_or_size[leader(a)]
-  end
-
-  def groups
-    (0 ... @parent_or_size.size).group_by{ |i| leader(i) }.values
-  end
-
-  def to_s
-    "<#{self.class}: @n=#{@n}, #{@parent_or_size}>"
-  end
-end
-UnionFind = DSU
 
 main
