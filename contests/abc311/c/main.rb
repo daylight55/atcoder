c12311f916f5933b9096975e2c5dd460627b6db1
daylight55 <jojomasanori3@gmail.com>
2023-07-29 08:06:13 +0900

    abc311: solve c

--- contests/abc311/c/main.rb
+++ contests/abc311/c/main.rb
@@ -1,83 +1,39 @@
 #!/usr/bin/env ruby
+def dfs(pos, graph, visited, path)
+  if visited[pos] # すでに訪れている場合は、cycleがあるということ
+    cycle = path[path.index(pos)..-1] # cycleの部分を抜き出す
+    return cycle
+  end
+
+  visited[pos] = true # 訪れたことを記録
+  path << pos # 訪れた順番を記録
+
+  graph[pos].each do |next_pos|
+    cycle = dfs(next_pos, graph, visited, path) # cycleを返す
+    return cycle if cycle
+  end
+end
+
 def main
   n = gets.chomp.to_i
-  graph = gets.chomp.split.map(&:to_i)
-  uf = UnionFind.new(n + 1)
+  a = gets.chomp.split.map(&:to_i)
+  graph = Array.new(n+1) { [] }
+  a.each_with_index { |v, i| graph[i] << v }
+  graph.unshift([0]) # インデックスとノード番号を合わせるために、0を先頭に追加する
 
-  graph.each_with_index{ |g, i|
-    uf.merge(i+1, g)
-  }
+  visited = Array.new(n+1, false) # インデックスとノード番号を合わせるために、n+1個の配列を用意する
+  visited[0] = true # 0は使わないので、trueにしておく
 
-  pp uf.groups
-
-  # 閉路の長さ
-  m = uf.groups[1].size
-  # 閉路の例を出力
-  ans = uf.groups[1]
-
-  puts m
-  puts ans.join(" ")
-end
-
-# ---------------------------------------------------
-# ref. https://github.com/universato/ac-library-rb/blob/main/lib/dsu.rb
-class DSU
-  def initialize(n = 0)
-    @n = n
-    @parent_or_size = Array.new(n, -1)
-    # root node: -1 * component size
-    # otherwise: parent
-  end
-
-  attr_reader :parent_or_size, :n
-
-  def merge(a, b)
-    x = leader(a)
-    y = leader(b)
-    return x if x == y
-
-    x, y = y, x if -@parent_or_size[x] < -@parent_or_size[y]
-    @parent_or_size[x] += @parent_or_size[y]
-    @parent_or_size[y] = x
-  end
-  alias unite merge
-
-  def same?(a, b)
-    leader(a) == leader(b)
-  end
-  alias same same?
-
-  def leader(a)
-    unless 0 <= a && a < @n
-      raise ArgumentError.new, "#{a} is out of range (0...#{@n})"
+  # 全てのノードからdfsを開始する
+  (1..n).each do |v|
+    next if visited[v] # すでに訪れている場合は、次のノードへ
+    cycle = dfs(v, graph, visited, [])
+    if cycle
+      puts cycle.size
+      puts cycle.join(' ')
+      exit
     end
-
-    @parent_or_size[a] < 0 ? a : (@parent_or_size[a] = leader(@parent_or_size[a]))
-  end
-  alias root leader
-  alias find leader
-
-  def [](a)
-    if @n <= a
-      @parent_or_size.concat([-1] * (a - @n + 1))
-      @n = @parent_or_size.size
-    end
-
-    @parent_or_size[a] < 0 ? a : (@parent_or_size[a] = self[@parent_or_size[a]])
-  end
-
-  def size(a)
-    -@parent_or_size[leader(a)]
-  end
-
-  def groups
-    (0 ... @parent_or_size.size).group_by{ |i| leader(i) }.values
-  end
-
-  def to_s
-    "<#{self.class}: @n=#{@n}, #{@parent_or_size}>"
   end
 end
-UnionFind = DSU
 
 main
