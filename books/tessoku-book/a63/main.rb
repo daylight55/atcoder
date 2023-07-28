015fa4f1e4e53b011f68e4bbb9091b1ed7df1094
daylight55 <jojomasanori3@gmail.com>
2023-07-29 08:04:54 +0900

    tessoku solve 063

--- books/tessoku-book/a63/main.rb
+++ books/tessoku-book/a63/main.rb
@@ -1,2 +1,25 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split
+n, m = gets.chomp.split.map(&:to_i)
+graph = Array.new(n+1) { [] }
+m.times do |i|
+  a, b = gets.chomp.split.map(&:to_i)
+  graph[a] << b
+  graph[b] << a
+end
+
+dist = Array.new(n+1, -1)
+queue = []
+dist[1] = 0
+queue << 1
+
+while !queue.empty?
+  pos = queue.shift
+  graph[pos].each do |to|
+    next if dist[to] != -1
+    dist[to] = dist[pos] + 1
+    queue << to
+  end
+end
+
+dist.shift
+puts dist
