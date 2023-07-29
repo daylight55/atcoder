694a3cd87b6b78b0b1f5fa46924373b0c5d09a5b
daylight55 <jojomasanori3@gmail.com>
2023-07-29 21:49:01 +0900

    wip

--- contests/abc312/c/main.rb
+++ contests/abc312/c/main.rb
@@ -1,17 +1,21 @@
 #!/usr/bin/env ruby
 def main
   n, m = gets.chomp.split.map(&:to_i)
-end
+  a = gets.chomp.split.map(&:to_i)
+  b = gets.chomp.split.map(&:to_i)
 
-# ---------------------------------------------------
-# 以下、自前ライブラリ
-class Array
-  # ref. https://stackoverflow.com/questions/1475808/cumulative-array-sum-in-ruby
-  def cumulative_sum
-    sum = 0
-    self.map{|x| sum += x}.unshift(0) # 頭に0を追加しておく
-  end
+  # Aの売り手が、ちょうどBの買い手より大きくなる時の最小値を求める
+
+  # a, bの各要素の差を取った配列を作成
+  diff = a.zip(b).map { |a, b| a - b }
+  # diffが最小値になった時の組み合わせとなったbのindexを全て取得
+  min_diff_index = diff.each_index.select { |i| diff[i] == diff.min }
+  # min_diff_indexの中で、bの値が最大のものを取得
+  max_b_index = min_diff_index.max_by { |i| b[i] }
+  # max_b_indexの時のAの値で条件分岐
+  
+
+
 end
 
 main
-
