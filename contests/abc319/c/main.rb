c092237f0bf3180cb875ec9dcd48a9a5c5495e96
daylight55 <jojomasanori3@gmail.com>
2023-09-10 23:26:26 +0900

    wip

--- contests/abc319/c/main.rb
+++ contests/abc319/c/main.rb
@@ -2,43 +2,28 @@
 def main
   grid = Array.new(3) { gets.chomp.split.map(&:to_i) }
 
-  # 3×3のマスを重複なく全てランダムに選択する全事象
-  all_pattern = (1..9).to_a.permutation.to_a
-  # 全事象の数
-  all_count = all_pattern.size
+  # 行、列、 対角線
+  rows = grid
+  cols = grid.transpose
+  diagonals = [[grid[0][0], grid[1][1], grid[2][2]], [grid[0][2], grid[1][1], grid[2][0]]]
 
-  # 高橋くんががっかりするパターン数
-  dessapoint_count = 0
+  # 行、列、対角線で同じ数字が2つある数を調べる
+  danger_count = 0
+  danger_count += rows.count { |row| row.uniq.size == 2 }
+  danger_count += cols.count { |col| col.uniq.size == 2 }
+  danger_count += diagonals.count { |diagonal| diagonal.uniq.size == 2 }
 
-  # 全事象で調べる
-  all_pattern.each do |pattern|
-    # 初めに2つ選択する座標のみ取り出す
-    first_select = pattern.each_slice(3).to_a.slice(0, 2)
+  # 各行、列、対角線で選ぶ事象数は9C3 = 84通り。同じ数字を連続して選び、かつそのラインの最後の数字を選ぶ事象数は3C2 = 6通り。かつ、その後の6マスの選択パターンは6!。以上よりがっかりするパターンは上記の数分かけて以下のように考えれば良い。
+  dissapoint_pattern = 9 * danger_count * 6 * 5 * 4 * 3 * 2 * 1  * danger_count
 
-    # 高橋くんのがっかりステータス
-    takahashi_wakuwaku = true
+  # 3×3のマスを全て選ぶパターンは9!通り
+  all_pattern = 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
 
-    # がっかりするパターンを見ていく
-    # 各行、各列について2つの数字が等しく、最後が異なるか
-    (0..2).each do |i|
-      rows = [grid[i][0], grid[i][1], grid[i][2]]
-      cols = [grid[0][i], grid[1][i], grid[2][i]]
+  pp dissapoint_pattern
+  pp all_pattern
 
-      if (rows.count(pattern[i]) == 2 && rows.count(pattern[i + 1]) == 1) ||
-
-
-
-
-  # がっかりせずに済む確率
-  not_dessapoint_probability = all_count - dessapoint_count / all_count.to_f
-
-  # 絶対誤差が10^−8以下になるように出力
-  puts not_dessapoint_probability.round(8)
-
-
-
-
+  # がっかりせず選択する確率を少数第8位まで求める
+  puts (all_pattern - dissapoint_pattern).to_f / all_pattern.to_f
 end
-
 
 main
