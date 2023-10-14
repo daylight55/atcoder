1c18083465a123b2594c5fe5e4e8f6ade1cd2157
daylight55 <jojomasanori3@gmail.com>
2023-10-14 21:22:16 +0900

    wip

--- contests/abc324/b/main.rb
+++ contests/abc324/b/main.rb
@@ -1,30 +1,20 @@
 #!/usr/bin/env ruby
-require 'prime'
 
 def main
   n = gets.chomp.to_i
 
-  # (1..50).each do |i|
-  #   if n % (2 ** i) == 0
-  #     puts("Yes")
-  #     return
-  #   end
-  # end
-  # puts("No")
-
-  factors = Prime.prime_division(n)
-
-  if factors.count == 1 && (factors[0][0] == 2 || factors[0][0] == 3)
-    puts("Yes")
-    return
+  while n % 2 == 0
+    n /= 2
+  end
+  while n % 3 == 0
+    n /= 3
   end
 
-  if factors.count == 2 && factors[0][0] == 2 && factors[1][0] == 3
-    puts("Yes")
-    return
+  if n == 1
+    puts "Yes"
+  else
+    puts "No"
   end
-
-  puts("No")
 end
 
 main
