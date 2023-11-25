e25cea50df69fc5e54562d0798a2bf37015448d2
daylight55 <jojomasanori3@gmail.com>
2023-11-25 21:48:13 +0900

    abc320: wip d

--- contests/abc330/d/main.rb
+++ contests/abc330/d/main.rb
@@ -1,17 +1,23 @@
 #!/usr/bin/env ruby
 def main
-  n, m = gets.chomp.split.map(&:to_i)
-end
+  n = gets.chomp.to_i
+  grid = Array.new(n){ gets.chomp.split('') }
 
-# ---------------------------------------------------
-# 以下、自前ライブラリ
-class Array
-  # ref. https://stackoverflow.com/questions/1475808/cumulative-array-sum-in-ruby
-  def cumulative_sum
-    sum = 0
-    self.map{|x| sum += x}.unshift(0) # 頭に0を追加しておく
+  # 条件
+  # 1. 組に含まれる3 マスは相異なる。
+  # 2. 3 マス全てに o が書かれている。
+  # 3. マスのうち、丁度2 つが同じ行にある。
+  # 4. マスのうち、丁度2 つが同じ列にある。
+  count = 0
+  (0..n-1).each do |i|
+    (0..n-1).each do |j|
+      if grid[i][j] == 'o'
+        count += 1 if grid[i][j+1] == 'o' && grid[i][j+2] == 'o'
+        count += 1 if grid[i+1][j] == 'o' && grid[i+2][j] == 'o'
+      end
+    end
   end
+
 end
 
 main
-
