290783569468f7bd000c82cf7cf25c9c7ba171f7
daylight55 <jojomasanori3@gmail.com>
2023-11-25 22:28:02 +0900

    abc320: solve c

--- contests/abc330/c/main.rb
+++ contests/abc330/c/main.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
 def main
   d = gets.chomp.to_i
-  min = d
+  min = Float::INFINITY
+  x = 0
 
-  (0..Math.sqrt(d).to_i).each do |x|
-    y = Math.sqrt(d - x**2).to_i
+  while x**2 <= d
+    y = Math.sqrt(d - x**2).round
     min = [min, (d - x**2 - y**2).abs].min
     if min == 0
       puts min
       exit
     end
+    x += 1
   end
 
   puts min
