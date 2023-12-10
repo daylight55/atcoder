5791585bb347b55dc0e1762a8b0d11c9d3a8f108
daylight55 <jojomasanori3@gmail.com>
2023-12-10 21:22:37 +0900

    wip

--- contests/abc332/c/main.rb
+++ contests/abc332/c/main.rb
@@ -1,17 +1,36 @@
 #!/usr/bin/env ruby
 def main
   n, m = gets.chomp.split.map(&:to_i)
-end
+  s = gets.chomp.split("").map(&:to_i)
 
-# ---------------------------------------------------
-# 以下、自前ライブラリ
-class Array
-  # ref. https://stackoverflow.com/questions/1475808/cumulative-array-sum-in-ruby
-  def cumulative_sum
-    sum = 0
-    self.map{|x| sum += x}.unshift(0) # 頭に0を追加しておく
+  desire = 0
+  logo_t_shirt_stock = 0
+  muji_t_shirt_stock = m.dup
+  dp = Array.new(n, 0)
+
+  while true
+    # ロゴ入りTシャツの数を1つずつ増やして確認していく
+    desire += 1
+
+    s.each do |i|
+      case i
+      # なんの予定もない = Tシャツを着用せず、シャツをすべて洗濯する
+      when 0
+        dp[]
+
+        logo_t_shirt_count = desire
+        muji_t_shirt_count = m
+      # 食事にいく = ロゴ入りTシャツ または 無地Tシャツを着用する
+      when 1
+        logo_t_shirt_count += 1
+      when 2
+        muji_t_shirt_count += 1
+      end
+    end
   end
+
+  puts desire
+
 end
 
 main
-
